692ed982a6effb153ccaf562a9ed5bd471e1cb5d
Mr.Sadiya Tech <sadiyatech33@gmail.com>
2024-10-04 23:29:39 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -4,12 +4,12 @@
 ▒█▄▄▄█ ▒█░▒█ ▒█▄▄▀ ▄█▄ ░░▒█░░ ▒█░▒█ ░░ ▒█░░▒█ ▒█▄▄▀
 
    * @project_name : SADIYA-MD
-   * @authors : SADIYA-MD(sadiya) <https://github.com/Sadiya70/SADIYA-MD>
-   * @youtube : https://www.youtube.com/@sadiyaofficial44
+   * @authors : SADIYA-MD(sadiya) <https://github.com/Mr-Sadiya-Tech/SADIYA-MD>
+   * @youtube : https://www.youtube.com/@Sadiya-Tech
    * @description : SADIYA-MD ,A Multi-functional whatsapp user bot.
-   * @version 1.5.0
+   * @version 4.5.0
 
-   * Created By SADIYA OFC.
+   * Created By Mr.Sadiya Tech.
    * © 2024 SADIYA-MD.
 */
 
